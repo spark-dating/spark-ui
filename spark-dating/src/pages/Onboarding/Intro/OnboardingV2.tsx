7f5e5d37e9a9c74a4b31e3d41c4dd03c42df602e
utyvert <utkarshuppal@gmail.com>
2023-05-28 19:55:33 -0700

    onboarding v2** work

--- spark-dating/src/pages/Onboarding/Intro/OnboardingV2.tsx
+++ spark-dating/src/pages/Onboarding/Intro/OnboardingV2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import SecondaryButton from '../../../components/Buttons/SecondaryButton';
 import {
   useFonts,
@@ -18,35 +18,50 @@ const OnboardingV2 = ({ navigation }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.topView}>
-        <Text style = {styles.header}>Experience the <Text style = {{fontFamily: 'OpenSans_700Bold'}}>future of love</Text></Text>
-        <Text style = {styles.subHeader}>Our AI-powered dating app redefines connections, turning digital encounters into lasting romance.</Text>
+    <ImageBackground
+      source={require('spark-dating/assets/Onboardingv2.jpg')}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <View style={styles.topView}>
+            <Text style={[styles.text, {display:'flex',  alignSelf: 'center', marginTop: -40, marginBottom: 40}]}>Spark</Text>
+          <Text style={styles.header}>
+            Experience the <Text style={{ fontFamily: 'OpenSans_700Bold' }}>future of love</Text>
+          </Text>
+          <Text style={styles.subHeader}>
+            Our AI-powered dating app redefines connections, turning digital encounters into lasting romance.
+          </Text>
+        </View>
+        <View style={styles.bottomView}>
+          <SecondaryButton style={styles.button} onPress={() => navigation.navigate('Login')}>
+            Sign up
+          </SecondaryButton>
+          <Text style={[styles.text, { marginTop: 14 }]}>
+            Existing account? <Text style={{ color: '#24786D' }}>Log in</Text>
+          </Text>
+        </View>
       </View>
-      <View style={styles.bottomView}>
-        <SecondaryButton style={styles.button} onPress={() => navigation.navigate('Login')}>
-          Sign up
-        </SecondaryButton>
-        <Text style = {[styles.text, {marginTop: 14}]}>Existing account? <Text style = {{color: '#24786D'}}>Log in</Text></Text>
-      </View>
-    </View>
+    </ImageBackground>
   );
 };
 
 export default OnboardingV2;
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'space-between',
-    // padding: 70, // padding to push the contents away from the edges
   },
   topView: {
     justifyContent: 'center',
+    width: '90%',
     paddingTop: 70, 
-    width: '90%'
   },
   bottomView: {
     width: '100%',
@@ -68,6 +83,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     lineHeight: 26,
     fontFamily: 'OpenSans_400Regular',
-    marginTop: 18
+    marginTop: 18,
   }
 });
+
